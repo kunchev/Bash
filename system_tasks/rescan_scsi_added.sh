c765b41d02838f1717f8e4c6c257d4c5b45b2c9b
unknown <ptkunchev@gmail.com>
2020-06-21 18:16:53 +0300

    Updated script

--- system_tasks/rescan_scsi_added.sh
+++ system_tasks/rescan_scsi_added.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-# rescan_scsi_added.sh
 
+
+# About: rescan_scsi_added.sh
+#
 # Rescan scsi bus on your machine for newly added disks using this script
 # Just execute ./rescan_scsi_added.sh and check the results
 # Note that if you remove disks this script will still show them as present
 
-# Revision:
+# Version log:
 # ver. 1.0.1 - Jul/21/2020
 # ver. 1.0.0 - Jan/24.2018
 
 # License: GPLv2
-# https://github.com/kunchev/Linux-Shell-Scripts/blob/master/LICENSE.md
 
-# Get block device list function:
+
 function list_blk() {
-    local blklist=`lsblk`
+    # Get block device list function - Linux, current block devices
+    local blklist=$(lsblk)
     echo "----$1:"
     printf "%s\n" "${blklist}"
+    echo ""
 }
 
-# Rescan scsi bus function - Linux, when adding a new disk:
 function rescan_scsi_bus_added() {
+    # Rescan scsi bus function - Linux, when adding a new disk
     local scsihost="/sys/class/scsi_host/host*/scan"
     for BUS in ${scsihost}
     do
@@ -30,16 +33,18 @@ function rescan_scsi_bus_added() {
 }
 
 function main() {
+    # Clear screen and call the list_blk() and rescan_blk() functions 
     clear
-    list_blk "Current block devices"
+    list_blk "Current block devices on $(hostname -s)"
+    echo -e "scanning...\n"
     rescan_scsi_bus_added
-    list_blk "Rescanned blk devs"
+    list_blk "Rescanned block devices on $(hostname -s)"
 }
 
 
-# run:
+# check if running as root and call main() if so, else exit
 if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run as root, exiting..!!" 1>&2
+    echo "This script must be run as root, you are $(whoami), exiting..!!" 1>&2
     exit 1
 else
     main
