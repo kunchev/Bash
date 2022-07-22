53278fc08f9c00dbc1876f2f24f25f3bb1003dff
Petyo Kunchev <kunchev@users.noreply.github.com>
2022-07-22 11:00:43 +0300

    Added -y in dnf install statement

--- install_vscode_fedora.sh
+++ install_vscode_fedora.sh
@@ -6,13 +6,15 @@ set -e
 # VSCode installation script for RHEL, Fedora, and CentOS based distros
 
 # Repository setup
+echo "Configuring repository..."
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 # VSCode installation
 dnf check-update
-sudo dnf install code
+sudo dnf install code -y
 
+# Check status of setup
 if [ $? -eq 0 ]
 then
   echo "Setup completed."
